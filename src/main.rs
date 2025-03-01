003f82e9e331c59563d327ee259cd28e9991fc9e
Jack O'Connor <oconnor663@gmail.com>
2025-02-28 16:20:50 -0800

    command line parser and progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
+use clap::Parser;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    path: Option<PathBuf>,
+}
+
 const IMAGE_WIDTH: u64 = 256;
 const IMAGE_HEIGHT: u64 = 256;
 
-fn main() {
-    print!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n");
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let mut outfile: Box<dyn Write>;
+    let bar;
+    if let Some(path) = args.path {
+        outfile = Box::new(BufWriter::new(File::create(path)?));
+        bar = None;
+    } else {
+        outfile = Box::new(std::io::stdout());
+        bar = Some(indicatif::ProgressBar::new(IMAGE_HEIGHT));
+    };
+
+    write!(outfile, "P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n")?;
 
     for j in 0..IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH {
@@ -14,7 +36,14 @@ fn main() {
             let ig = (255.999 * g) as u64;
             let ib = (255.999 * b) as u64;
 
-            println!("{ir} {ig} {ib}");
+            writeln!(outfile, "{ir} {ig} {ib}")?;
+        }
+        if let Some(bar) = &bar {
+            bar.inc(1);
         }
     }
+
+    outfile.flush()?;
+
+    Ok(())
 }
